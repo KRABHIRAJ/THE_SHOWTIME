4730bb5421cd3e791b2adf6b795a46b3d9773b01
Abhiraj Kumar <kumarshishu99@gmail.com>
2025-01-10 11:12:24 +0530

    added carousel detail

--- src/components/home/carousel/CarouselContainer.jsx
+++ src/components/home/carousel/CarouselContainer.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { getImageCdnUrl } from "../../../utils/constants";
+import CarouselDetails from "./CarouselDetails";
 
 const CarouselContainer = () => {
   const nowPlayingMovies = useSelector((state) => state.app.nowPlayingMovies);
@@ -8,21 +9,24 @@ const CarouselContainer = () => {
   useEffect(() => {
     const inter = setInterval(() => {
         setCarouselIndex((currIndex) => {
-            if(currIndex === nowPlayingMovies.length - 1){
+            if(currIndex >= nowPlayingMovies.length - 1){
                 return 0;
             }else{
                 return currIndex + 1;
             }
         })
-    }, 4000)
+    },10000)
 
     return () => {
         clearInterval(inter)
     }
   }, [])
   return (
-    <div className="h-[500px] w-screen">
-        <img className="h-full w-full object-cover" src={getImageCdnUrl('original', nowPlayingMovies[carouselIndex]?.backdrop_path)} alt="bannner_img" />
+    <div className="h-[500px] w-screen relative">
+        <img className="h-full w-full object-cover opacity-[0.4]" src={getImageCdnUrl('original', nowPlayingMovies[carouselIndex]?.backdrop_path)} alt="bannner_img" />
+        <div className="absolute bottom-[10%]">
+            <CarouselDetails data={nowPlayingMovies[carouselIndex]} />
+        </div>
     </div>
   )
 }
---
+++ src/components/home/carousel/CarouselDetails.jsx
@@ -0,0 +1,31 @@
+/* eslint-disable react/prop-types */
+
+import StarIcon from "@mui/icons-material/Star";
+import PlayCircleIcon from "@mui/icons-material/PlayCircle";
+import { getImageCdnUrl } from "../../../utils/constants";
+
+const CarouselDetails = ({ data }) => {
+  return (
+    <div className="flex items-center px-4 max-w-[75%] ">
+      <div className="flex-[0.2]">
+        <img className="w-[150px] h-[200px] rounded-lg object-cover"  src={getImageCdnUrl('original', data?.poster_path)} alt="poster_img" />
+      </div>
+      <div className=" flex-[0.8] pl-2">
+        <p className="font-bold text-2xl tracking-wide mb-2 line-clamp-1">{data?.original_title || data?.title}</p>
+        <div className="flex gap-x-1 text-sm items-center">
+          <StarIcon sx={{ color: "#facc15", fontSize: "20px" }} />
+          <p>{data?.vote_average.toFixed(1)}</p>
+        </div>
+        <p className="line-clamp-2 my-1 text-[15px]">{data?.overview}</p>
+        <p className="font-semibold text-sm">
+          Released : <span className="ml-1">{data?.release_date}</span>
+        </p>
+        <button className="bg-gray-600 p-2 px-4 mt-3 rounded-lg hover:bg-gray-700">
+          Watch Now <PlayCircleIcon />{" "}
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default CarouselDetails;
